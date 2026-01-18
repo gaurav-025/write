4de4a6c6fab9ee248ba44d81360e24e650cc0deb
gaurav-025 <royalsy99@gmail.com>
2026-01-18 17:50:55 +0530

    code fix

--- write/settings.py
+++ write/settings.py
@@ -96,14 +96,16 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND="django_sendgrid_backend.SendgridBackend" 
+SENDGRID_API_KEY=os.getenv("SENDGRID_API_KEY")
 
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
+EMAIL_HOST='api.sendgrid.com'
+EMAIL_PORT=443
+# EMAIL_USE_TLS=True
 
-EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
